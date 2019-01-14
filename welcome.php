eb5f4978acdc2829c20e9f208042927fb058b930
anjalidaryani <44505831+anjalidaryani@users.noreply.github.com>
2019-01-13 16:56:26 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -3,6 +3,7 @@
 
 Thank you <?php echo $_POST["name"]; ?> for providing your contact information.<br>
 We will send you email updates about our adventures at <?php echo $_POST["email"]; ?>
+ <button onclick="window.location.href = 'index.html';">Main Page</button>
 
 </body>
 </html>
