866fe16b557af5987755447df9d8a47c16e6054f
anjalidaryani <44505831+anjalidaryani@users.noreply.github.com>
2019-01-13 16:55:41 -0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -2,7 +2,7 @@
 <body>
 
 Thank you <?php echo $_POST["name"]; ?> for providing your contact information.<br>
-Your email address is: <?php echo $_POST["email"]; ?>
+We will send you email updates about our adventures at <?php echo $_POST["email"]; ?>
 
 </body>
 </html>
